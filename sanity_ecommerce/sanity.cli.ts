e74226ea6c6bd821aa847cb0d34cf5f84a06b3e7
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2022-12-21 12:22:54 +0330

    feat(sanity): configure sanity with schemas

---
+++ sanity_ecommerce/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'evw7bjg9',
+    dataset: 'production'
+  }
+})
---
+++ sanity_ecommerce/sanity.config.ts
@@ -0,0 +1,23 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+//import {googleMapsInput} from '@sanity/google-maps-input'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'ecommerce',
+
+  projectId: 'evw7bjg9',
+  dataset: 'production',
+
+  plugins: [
+    deskTool(),
+    visionTool(),
+    //googleMapsInput(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
